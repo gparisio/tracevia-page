f738891807b2dcb9db21e2dc4275debe968104b8
Gabriel Oliveira Parísio <104593158+gparisio@users.noreply.github.com>
2025-11-26 18:31:48 -0300

    Update redirect to new home controller

--- tracevia-page/index.php
+++ tracevia-page/index.php
@@ -1,3 +1,4 @@
 <?php
 // Redireciona tudo para o controller principal da Home
-require __DIR__ . '/controllers/tracevia-home-novo-controller.php';
+require __DIR__ . '/controllers/tracevia-home-controller-novo.php';
+
